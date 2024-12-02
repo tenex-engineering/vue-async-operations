ec5477fd823a4ee740e4aed14e74ea647391720b
Chabib N. R. <cnr@tenex.engineering>
2024-12-03 02:30:16 +0700

    bundling

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
 
-export default defineConfig({})
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'dist/index.js'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['vue'],
+    },
+  },
+})
